dbfec80ddbb60c39d793602cef9efb71b2932c3c
Christian Lanz <christian.l4nz@gmail.com>
2020-01-13 14:24:25 +0100

    Add minor ui changes

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -9,16 +9,21 @@ function addStaker(staker) {
       `}
       <div class="float-left ml-3">
         <p class="text-left font-weight-bold"><span>${staker.name ? staker.name : "Unknown"}${staker.name && staker.keybasePubKey ? ` <i class="fas fa-check-circle verified"></i>` : ""}</span></p>
-        <p class="text-left font-weight-light">${staker.address}</p>
+        <p class="text-left font-weight-light address">${staker.address}</p>
       </div>
     </div>
     <div class="cell" data-title="Self-Staked"><p>${numeral(staker.selfStaked).format("0,0")} FTM</p></div>
-    <div class="cell" data-title="Delegated"><p>${numeral(staker.delegated).format("0,0")} FTM</p></div>
-    <div class="cell available" data-title="Available"><p>${numeral(staker.availableDelegationAmount).format("0,0")} FTM <span class="font-weight-light">(${numeral(staker.availableDelegationPercent).format("0.00%")})</span></p></div>
-    <div class="cell contact" data-title="Contact">
-    ${staker.contact ? `
-      <a href="${staker.contact}" target="_blank">
+    <div class="cell" data-title="Total Staked"><p>${numeral(staker.totalStaked).format("0,0")} FTM</p></div>
+    <div class="cell available" data-title="Available"><p>${numeral(staker.availableDelegationAmount).format("0,0")} FTM</p></div>
+    <div class="cell contact" data-title="">
+    ${staker.website ? `
+      <a class="mr-1" href="${staker.website}" target="_blank">
         <i class="fas fa-globe-americas link"></i>
+      </a>
+    ` : ""}
+    ${staker.contact ? `
+      <a class="ml-1" href="${staker.contact}" target="_blank">
+        <i class="fas fa-headset link"></i>
       </a>
     ` : ""}
     </div>
