00fc9b0db4c452b4279378db684a7f4d29694479
Christian Lanz <christian.l4nz@gmail.com>
2020-01-14 16:51:34 +0100

    Add switch to hide unknown validators

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,35 +1,35 @@
-function addStaker(staker) {
+function addValidator(validator) {
     const markup = `
-    <div class="cell" data-title="ID"><p>${staker.id}</p></div>
+    <div class="cell" data-title="ID"><p>${validator.id}</p></div>
     <div class="cell name" data-title="Name">
-      ${staker.isCheater ? `
+      ${validator.isCheater ? `
         <i class="fas fa-skull float-lg-left icon text-danger"></i>
       ` : `
-        ${staker.name ? `
-          ${staker.logoUrl ? `<img class="float-lg-left" src="${staker.logoUrl}">` : `<p class="float-lg-left icon"></p>`}
+        ${validator.name ? `
+          ${validator.logoUrl ? `<img class="float-lg-left" src="${validator.logoUrl}">` : `<p class="float-lg-left icon"></p>`}
         ` : `
           <i class="fas fa-question float-lg-left icon"></i>
         `}
       `
       }
       <div class="float-lg-left ml-lg-3 mt-sm-2 mt-lg-0">
-        <p class="text-lg-left ${staker.name ? "font-weight-bold" : ""} name"><span>${staker.name ? staker.name : "Unknown"}${staker.name && staker.isVerified ? ` <i class="fas fa-check-circle verified" title="Verified via Blockchain"></i>` : ""}</span></p>
-        <p class="text-lg-left font-weight-light address"><a href="https://explorer.fantom.network/validator/${staker.id}">${staker.address.toLowerCase()}</a></p>
+        <p class="text-lg-left ${validator.name ? "font-weight-bold" : ""} name"><span>${validator.name ? validator.name : "Unknown"}${validator.name && validator.isVerified ? ` <i class="fas fa-check-circle verified" title="Verified via Blockchain"></i>` : ""}</span></p>
+        <p class="text-lg-left font-weight-light address"><a href="https://explorer.fantom.network/validator/${validator.id}">${validator.address.toLowerCase()}</a></p>
       </div>
     </div>
-    <div class="cell" data-title="Self-Staked"><p>${numeral(staker.selfStaked).format("0,0")} FTM</p></div>
-    <div class="cell" data-title="Total Staked"><p>${numeral(staker.totalStaked).format("0,0")} FTM</p></div>
-    <div class="cell available" data-title="Available"><p>${numeral(staker.availableDelegationAmount).format("0,0")} FTM</p></div>
-    <div class="cell" data-title="Productivity"><p>${numeral(staker.productivity).format("0.00%")}</p></div>
+    <div class="cell" data-title="Self-Staked"><p>${numeral(validator.selfStaked).format("0,0")} FTM</p></div>
+    <div class="cell" data-title="Total Staked"><p>${numeral(validator.totalStaked).format("0,0")} FTM</p></div>
+    <div class="cell available" data-title="Available"><p>${numeral(validator.availableDelegationAmount).format("0,0")} FTM</p></div>
+    <div class="cell" data-title="Productivity"><p>${numeral(validator.productivity).format("0.00%")}</p></div>
     <div class="cell links" data-title="Links">
-    ${staker.website || staker.contact ? `
-      ${staker.website ? `
-        <a class="mr-1" href="${staker.website}" target="_blank">
+    ${validator.website || validator.contact ? `
+      ${validator.website ? `
+        <a class="mr-1" href="${validator.website}" target="_blank">
           <i class="fas fa-globe-americas link"></i>
         </a>
       ` : ""}
-      ${staker.contact ? `
-        <a class="ml-1" href="${staker.contact}" target="_blank">
+      ${validator.contact ? `
+        <a class="ml-1" href="${validator.contact}" target="_blank">
           <i class="fas fa-headset link"></i>
         </a>
       ` : ""}
@@ -47,10 +47,42 @@ function addStaker(staker) {
     document.querySelector(".table").appendChild(child);
 }
 
-(function ($) {
+function updateValidators() {
+  const hideUnknown = window.localStorage.getItem("hideUnknown");
+
+  // Fetch validators from backend
   axios.get("https://fantomstaker.info:44220/api/v1/validators").then((response) => {
-    response.data.forEach((staker) => {
-      addStaker(staker);
-    })
+    let validators = response.data;
+
+    if (hideUnknown == "true") {
+      validators = validators.filter((validator) => validator.name)
+    }
+
+    // Get all table rows 
+    const tableRows = document.querySelector(".table").children;
+    const tableRowCount = tableRows.length;
+
+    // Remove all table rows except the header row
+    for (i = 1; i < tableRowCount; i++) {
+      tableRows[1].remove();
+    }
+
+    // Add validator to the table
+    validators.forEach((validator) => { addValidator(validator); })
   })
-})(jQuery);
+}
+
+(function ($) {
+  let hideUnknown = window.localStorage.getItem("hideUnknown");
+
+  if (hideUnknown === null) {
+    window.localStorage.setItem("hideUnknown", "false");
+    hideUnknown = "false";
+  }
+
+  // Update switch state
+  document.querySelector("#hideUnknown").checked = hideUnknown == "true";
+
+  // Update validators
+  updateValidators();
+})();
