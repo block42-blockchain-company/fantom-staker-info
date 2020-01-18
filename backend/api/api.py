29e5e58e728ef6cd6f75bc5ab8bfb261b295ebd2
Christian Lanz <christian.l4nz@gmail.com>
2020-01-18 12:08:01 +0100

    Add onlyKnown request arg to validators api

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,11 +1,13 @@
 import json
-
 import flask
+
+from flask import request
 from flask_cors import CORS
 from tinydb import TinyDB
+from tinydb import Query
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = False
+app.config["DEBUG"] = True
 CORS(app)
 
 
@@ -16,12 +18,24 @@ def home():
 
 @app.route('/api/v1/general', methods=['GET'])
 def general():
-    return json.dumps(TinyDB('../db.json').table('general').all()[0])
+    general = TinyDB('../db.json').table('general')
+    data = general.all()[0]
+
+    return json.dumps(data)
 
 
 @app.route('/api/v1/validators', methods=['GET'])
 def validators():
-    return json.dumps(TinyDB('../db.json').table('validators').all())
+    onlyKnown = request.args.get('onlyKnown')
+
+    validators = TinyDB('../db.json').table('validators')
+
+    if onlyKnown is not None and onlyKnown.lower() == 'true':
+        data = validators.search(Query().name != '')
+    else:
+        data = validators.all()
+
+    return json.dumps(data)
 
 
 if app.config["DEBUG"]:
