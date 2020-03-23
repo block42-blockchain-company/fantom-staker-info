a8d9bb6189535b639ab539de0f7e0399f3f6f244
Christian Lanz <christian.l4nz@gmail.com>
2020-03-23 20:21:57 +0100

    Add minor change

--- backend/update.py
+++ backend/update.py
@@ -6,5 +6,7 @@ from App import App
 FIFTEEN_MINUTES_IN_SECONDS = 15 * 60
 
 while True:
+    print("### Sync started ###")
     App.run()
+    print("### Sync finished ###")
     sleep(FIFTEEN_MINUTES_IN_SECONDS)
